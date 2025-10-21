dd43c63f99067a9589961f6747398bfe72e196d9
Maximilian Walsh <max.walsh@gmail.com>
2025-10-21 16:59:36 +0200

    Anpassung Pausenzeiten, Sekundenansicht wurde entfernt

--- frontend/app.js
+++ frontend/app.js
@@ -111,8 +111,9 @@ document.addEventListener("DOMContentLoaded", () => {
         const now = getSynchronizedDate();
         const hours = String(now.getHours()).padStart(2, "0");
         const minutes = String(now.getMinutes()).padStart(2, "0");
-        const seconds = String(now.getSeconds()).padStart(2, "0");
-        clockElement.textContent = `${hours}:${minutes}:${seconds}`;
+//        const seconds = String(now.getSeconds()).padStart(2, "0");
+        clockElement.textContent = `${hours}:${minutes}`;
+//        clockElement.textContent = `${hours}:${minutes}:${seconds}`;
 
         checkBreakStatus(now);
     }
